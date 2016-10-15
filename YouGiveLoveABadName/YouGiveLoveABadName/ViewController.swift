1b729c75029c1792f47cc26cb88e053b20e29d6d
EnriqueTor <etorrendell@gmail.com>
2016-10-15 00:50:30 -0400

    Complete but needs improvement

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,16 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bonImage: UIImageView!
+    
+    @IBOutlet weak var bonSmall: NSLayoutConstraint!
+    @IBOutlet weak var bonMedium: NSLayoutConstraint!
+    @IBOutlet weak var bonL: NSLayoutConstraint!
+    @IBOutlet weak var bonXL: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bonMedium.isActive = true
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         
-
+        self.bonImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        UIView.animateKeyframes(withDuration: 0.35, delay: 0.0, options: .calculationModePaced, animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3, animations: {
+                self.bonMedium.isActive = false
+                self.bonSmall.isActive = true
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
+                self.bonSmall.isActive = false
+                self.bonXL.isActive = true
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3, animations: {
+                self.bonXL.isActive = false
+                self.bonL.isActive = true
+                self.view.layoutIfNeeded()
+            })
+            
+            }, completion: nil)
     }
-
 }
+
+
+
 
